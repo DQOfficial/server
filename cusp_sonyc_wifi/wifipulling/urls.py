a43156e64bb41b48c812d625ae6b353e200c3f6c
DQOfficial <dan.quasney@gmail.com>
2016-06-04 16:27:59 -0400

    updated urls with new views

--- cusp_sonyc_wifi/wifipulling/urls.py
+++ cusp_sonyc_wifi/wifipulling/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import patterns, url
 from wifipulling import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'))
+    url(r'^$', views.index, name='index'),
+    url(r'^$',views.index_2, name='index_2'),
+    url(r'^$',views.aggregate, name='aggregate'))
