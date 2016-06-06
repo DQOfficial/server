ba69b34ee3c8de6f9cc149e99455e45fd21f80dd
DQOfficial <dan.quasney@gmail.com>
2016-06-06 10:53:05 -0400

    update urls.py

--- cusp_sonyc_wifi/wifipulling/urls.py
+++ cusp_sonyc_wifi/wifipulling/urls.py
@@ -3,5 +3,4 @@ from wifipulling import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^$',views.index_2, name='index_2'),
-    url(r'^$',views.aggregate, name='aggregate'))
+    url(r'aggregate$',views.aggregate,name='aggregate')
